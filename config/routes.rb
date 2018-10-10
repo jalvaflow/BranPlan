a8ebe28bbbd42c602bb9afb205cf10497228438e
athena.xcy <athena.xcy@gmail.com>
2018-10-10 07:37:50 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+ 
   root 'static_pages#home'
+
+  get 'help', to: "static_pages#help" 
+
+  get 'contact', to: "static_pages#contact"
+
+  get 'about', to: "static_pages#about"
+
 end
