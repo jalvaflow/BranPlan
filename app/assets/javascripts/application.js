2d0eba3cc2de9af403b6a15efc84b8d679f94e44
Joshua <JoshuaAlva7@gmail.com>
2018-10-23 00:45:19 -0400

    Added cards for main features (navbar needs fixing)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery_ujs
 //= require jquery
+//= require turbolinks
 //= require bootstrap
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
