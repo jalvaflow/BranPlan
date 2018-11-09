c6d20d9a6eacbf91badb4a00ca4b34046d6f804d
Joshua <JoshuaAlva7@gmail.com>
2018-11-08 21:50:11 -0500

    Changed appl.rb

--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,12 @@ module Branplan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.test_framework :minitest, spec: true
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
-end
-
-config.generators do |g|
-  g.test_framework :minitest, spec: true
 end
