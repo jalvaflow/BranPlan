b21a12d87821cc99371a958c0be05c7cb500010a
Joshua <JoshuaAlva7@gmail.com>
2018-11-27 18:48:39 -0500

    Edit user model for OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,11 @@ class User < ApplicationRecord
   has_many :courses, through: :enrollments
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
-    end
+    user = find_or_create_by(provider: auth.provider, uid: auth.uid)
+    user.name = auth.info.name
+    user.oauth_token = auth.credentials.token
+    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    user.save!
+    return user
   end
 end
